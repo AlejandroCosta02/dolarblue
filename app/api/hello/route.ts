25e388c185e9e00fff2f9116a7b660142db7467a
Alejandro Costa <costa.claudio.alejandro@gmail.com>
2023-05-05 21:19:32 -0300

    update version and new dolar heropage

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,16 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
-};
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: "John Doe" });
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ message: "Hello world!" });
 }
+
 // import type { NextApiRequest, NextApiResponse } from "next";
 // import { PrismaClient } from "@prisma/client";
 
